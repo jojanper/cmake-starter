b8b3ca37d40f2143086c6cd1739310cc5acc32a0
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-12-22 20:35:55 +0200

    utils.cmake module.

--- cmake/library.cmake
+++ cmake/library.cmake
@@ -1,22 +1,3 @@
-# Parse list such that first argument is assigned to FIRST_ARG variable
-# and remaining arguments are assinged to REMAINING_ARG variables. These
-# variables are available to the the caller via PARENT_SCOPE.
-function(parse_list)
-  set(args ${ARGN})
-  list(LENGTH args num_args)
-  MATH(EXPR list_length "${num_args}-1")
-
-  set(lib_srcs)
-  foreach(arg RANGE 1 ${list_length})
-    list(GET args ${arg} entry )
-    set(lib_srcs "${lib_srcs} ${entry}")
-  endforeach(arg)
-
-  list(GET args 0 lib_name)
-  set(FIRST_ARG ${lib_name} PARENT_SCOPE)
-  set(REMAINING_ARG ${lib_srcs} PARENT_SCOPE)
-endfunction()
-
 # Create object library.
 #
 # lib_name - Name of library
