26bd3ac4fef0889384648cf849ae4ba343f24807
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-12-28 16:59:58 +0200

    Clang support.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 BUILD_TYPE=${1-'Debug'}
 BUILD_DIR=${2-'build'}
 CWD=`pwd`
+OSNAME=`uname`
 
 rm -Rf ${BUILD_DIR}/*
 mkdir -p ${BUILD_DIR}
@@ -19,10 +20,10 @@ rm -Rf zip_package
 mkdir -p zip_package
 cd zip_package
 rm -Rf *
-unzip ../cdraaloptions-Linux-${BUILD_TYPE}-0.1.0-shared.zip
+unzip ../cdraaloptions-${OSNAME}-${BUILD_TYPE}-0.1.0-shared.zip
 rm -Rf ${BUILD_DIR}/*
 mkdir -p ${BUILD_DIR}
-cmake -H../../src/tests/public -B./${BUILD_DIR} -DPACKAGE_PREFIX=${CWD}/build/zip_package -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake -H../../src/tests/public -B./${BUILD_DIR} -DPACKAGE_PREFIX=${CWD}/${BUILD_DIR}/zip_package -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 cd ${BUILD_DIR}
 cmake --build . --config ${BUILD_TYPE}
 
@@ -34,9 +35,9 @@ rm -Rf zip_package
 mkdir -p zip_package
 cd zip_package
 rm -Rf *
-unzip ../cdraaloptions-Linux-${BUILD_TYPE}-0.1.0-static.zip
+unzip ../cdraaloptions-${OSNAME}-${BUILD_TYPE}-0.1.0-static.zip
 rm -Rf ${BUILD_DIR}/*
 mkdir -p ${BUILD_DIR}
-cmake -H../../src/tests/public -B./${BUILD_DIR} -DSHARED_PACKAGE=OFF -DPACKAGE_PREFIX=${CWD}/build/zip_package -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake -H../../src/tests/public -B./${BUILD_DIR} -DSHARED_PACKAGE=OFF -DPACKAGE_PREFIX=${CWD}/${BUILD_DIR}/zip_package -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 cd ${BUILD_DIR}
 cmake --build . --config ${BUILD_TYPE}
--- cmake/cxxflags.cmake
+++ cmake/cxxflags.cmake
@@ -1,10 +1,10 @@
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
 
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -Wno-unused-const-variable")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99 -fPIC")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -Wno-unused-const-variable -std=c99")
 
   if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections -Wno-missing-field-initializers -Wno-missing-braces")
@@ -16,3 +16,8 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--exclude-libs,ALL,--gc-sections")
   endif(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
 endif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections")
+endif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
