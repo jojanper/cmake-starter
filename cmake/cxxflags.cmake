00021a07924bf47c8bdc566901e78eb95d13f765
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-12-29 13:22:30 +0200

    Simplify GNU build flags.

--- cmake/cxxflags.cmake
+++ cmake/cxxflags.cmake
@@ -6,15 +6,13 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -Wno-unused-const-variable -std=c99")
 
-  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections -Wno-missing-field-initializers -Wno-missing-braces")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections -Wno-non-template-friend -Wno-missing-field-initializers -Wno-missing-braces")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--exclude-libs,ALL,--gc-sections")
-  else()
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections -Wno-error=shift-negative-value -Wno-error=misleading-indentation")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections -Wno-error=shift-negative-value -Wno-error=misleading-indentation -Wno-non-template-friend")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--exclude-libs,ALL,--gc-sections")
-  endif(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -ffunction-sections -fdata-sections")
+
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-missing-field-initializers -Wno-missing-braces")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-non-template-friend -Wno-missing-field-initializers -Wno-missing-braces")
+
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--exclude-libs,ALL,--gc-sections")
 endif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
